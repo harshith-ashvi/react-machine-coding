c3324e14819c111eef343bfde05313bc76672f8b
harshith <harshith.ashvi@gmail.com>
2026-01-05 07:32:04 +0530

    add onClickOutside hook in modal and update animation

--- src/app/modal/modal.tsx
+++ src/app/modal/modal.tsx
@@ -1,6 +1,9 @@
-import { cn } from "@/lib/utils";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import { X } from "lucide-react";
-import React from "react";
+import { motion } from "motion/react";
+import { cn } from "@/lib/utils";
 
 interface ModalProps {
   isOpen: boolean;
@@ -10,16 +13,44 @@ interface ModalProps {
   onClose: () => void;
 }
 
+const useClickOutside = (onClose: () => void) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+    document.addEventListener("click", handleClick);
+
+    return () => document.removeEventListener("click", handleClick);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return containerRef;
+};
+
 const Modal = ({ isOpen, title, className, content, onClose }: ModalProps) => {
+  const containerRef = useClickOutside(onClose);
+
   if (!isOpen) return null;
 
   return (
     <div className="w-full h-full bg-black/50 backdrop-blur-sm fixed z-10, inset-0 flex items-center justify-center">
-      <div
+      <motion.div
+        ref={containerRef}
         className={cn(
           "rounded-lg w-[80%] h-[80%] border border-neutral-600 ",
           className
         )}
+        initial={{ opacity: 0, width: 0, height: 0 }}
+        exit={{ opacity: 0, width: 0, height: 0 }}
+        animate={{ opacity: 1, height: "80%", width: "80%" }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
       >
         <div className="flex items-center justify-between px-4 py-2 border-b border-neutral-800">
           <h4 className="font-bold text-2xl">{title}</h4>
@@ -31,7 +62,7 @@ const Modal = ({ isOpen, title, className, content, onClose }: ModalProps) => {
           </button>
         </div>
         <div className="p-2">{content}</div>
-      </div>
+      </motion.div>
     </div>
   );
 };
